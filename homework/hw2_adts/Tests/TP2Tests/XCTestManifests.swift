fe47cce40e265ad8876150114979e5e99ebbe96f
Damien Morard <damienmorard@musee.cui.unige.ch>
2018-10-15 14:07:31 +0200

    Correction

--- homework/hw2_adts/Tests/TP2Tests/XCTestManifests.swift
+++ homework/hw2_adts/Tests/TP2Tests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(TP1LibTests.allTests),
+        testCase(TP2Tests.allTests),
     ]
 }
 #endif
